4081687dbef9a4fffa18e8fc2c629e2bc2f2b175
Rupesh Ranjan <2068799@cognizant.com>
2024-03-20 20:09:41 +0530

    Add project files.

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const dispalyMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    dispalyMessage('⛔ No Number');
+  } else if (guess === secretNumber) {
+    if (highScore < score) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    dispalyMessage('🎉 Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (score > 0) {
+    document.querySelector('.message').textContent =
+      guess > secretNumber ? '📈 Too High' : '📉 Too Low';
+    document.querySelector('.score').textContent = --score;
+  } else {
+    dispalyMessage('💥 You lost the game!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#ff1a1a';
+    document.querySelector('.number').style.width = '30rem';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  dispalyMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
