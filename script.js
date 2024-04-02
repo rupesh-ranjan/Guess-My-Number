669fa0477e0b67e7a744e95ebbfa456c480ed2bc
Rupesh Ranjan <2068799@cognizant.com>
2024-04-02 18:56:38 +0530

    Test

--- script.js
+++ script.js
@@ -44,3 +44,6 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+
+    // Test
